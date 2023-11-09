ca239f7b6c86fb1be0eb3279cf578b8bdeef3db4
Glen <glen.tanyonghao@gmail.com>
2023-11-09 10:25:28 +0800

    Updating Page 2 Content
    Updating the content for Page2.jsx. The issue with the images url duplicating has been solved by the team from PokeAPI.

--- Pokemon_App_Glen/src/pages/Page2.jsx
+++ Pokemon_App_Glen/src/pages/Page2.jsx
@@ -3,40 +3,60 @@ import { Link } from "react-router-dom";
 
 const Page2 = () => {
   return (
-    <div className="container">
-      <div className="about">
-        <div className="about-title">
-          <h3>Welcome to the Pokédex App!</h3>
-        </div>
-        <div className="about-body">
-          <div>
-            This App is created for Pokémon enthusiast and beginners alike. Use
-            the App to find information about all the Pokémons in the Pokémon
-            world, and use it in your gameplay or just for discussions with
-            friends!
+    <>
+      <div className="black-bar"></div>
+      <div className="container">
+        <br></br>
+        <div className="about">
+          <div className="about-title">
+            <h3>Welcome to the Pokédex App!</h3>
           </div>
-        </div>
+          <div className="about-body">
+            <div>
+              This App is created for Pokémon enthusiast and beginners alike.
+              Use the App to find information about all the Pokémons in the
+              Pokémon world, and use it in your gameplay or just for discussions
+              with friends!
+            </div>
+          </div>
 
-        <div className="about-title">
-          <h3>How to Use</h3>
-        </div>
-        <div className="about-body">
-          <div>
-            There are two ways you can go about searching for Pokémon.
-            <ul>
-              <li>
-                Use the searchbar and type any Pokémon name or an ID (from 1 to
-                1017) to display in the Pokédex
-              </li>
-              <li>
-                Click on any icons you see at the bottom half of the page to
-                display Pokémon in the Pokédex!
-              </li>
-            </ul>
+          <br></br>
+
+          <div className="about-title">
+            <h3>How to Use</h3>
+          </div>
+          <div className="about-body">
+            <div>
+              There are two ways you can go about searching for Pokémons.
+              <ul>
+                <li>
+                  Use the searchbar and type any Pokémon name or an ID (from 1
+                  to 1017) to display in the Pokédex.
+                </li>
+                <li>
+                  Click on any icons you see at the bottom half of the page to
+                  display the selected Pokémon in the Pokédex!
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <br></br>
+
+          <div className="about-title">
+            <h3>Credits</h3>
+          </div>
+          <div className="about-body">
+            <div>
+              This App taps on the data and information retrieved from the
+              <a href="https://pokeapi.co/">PokeAPI</a> website. Many thanks to
+              the team for their hardwork to create and maintain the database,
+              without which this App would not be fully functional.
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
